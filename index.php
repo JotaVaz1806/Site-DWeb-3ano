6cad34e8de5a71c7f488412ccda6860dbbd17b6f
Caio Henrique <Caio_Henrique2006@outlook.com>
2025-03-11 08:05:27 -0300

    .

--- index.php
+++ index.php
@@ -12,48 +12,9 @@ if (!isset($_SESSION['id'])) {
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Document</title>
-    <link rel="stylesheet" href="styleSobre.css" />
+    <link rel="stylesheet" href="./style/nav.css" />
+    <link rel="stylesheet" href="./style/geral.css" />
     <style>
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        font-family: Arial, sans-serif;
-      }
-
-      /* Estilização da barra de navegação */
-      nav {
-        background-color: #333;
-        display: flex;
-        justify-content: space-around;
-        align-items: center;
-        height: 60px;
-      }
-
-      /* Estilização dos links */
-      nav ul {
-        list-style: none;
-        display: flex;
-      }
-
-      nav ul li {
-        margin: 0 15px;
-      }
-
-      nav ul li a {
-        text-decoration: none;
-        color: white;
-        font-size: 18px;
-        padding: 8px 12px;
-        transition: 0.3s;
-      }
-
-      /* Efeito hover nos links */
-      nav ul li a:hover {
-        background-color: #555;
-        border-radius: 5px;
-      }
-
       .blog {
         display: flex;
         justify-content: center;
@@ -70,24 +31,6 @@ if (!isset($_SESSION['id'])) {
         margin: 10px;
         padding: 20px;
         border: 1px solid black;
-      }
-
-      /* Responsividade para telas menores */
-      @media (max-width: 600px) {
-        nav {
-          flex-direction: column;
-          height: auto;
-          padding: 10px 0;
-        }
-
-        nav ul {
-          flex-direction: column;
-          align-items: center;
-        }
-
-        nav ul li {
-          margin: 10px 0;
-        }
       }
     </style>
   </head>
